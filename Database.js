610b66d445bf3bebc0b1c082ba6a17cde1c5cf01
Rahul Kumar Yadav <140547910+rahullnct@users.noreply.github.com>
2025-08-03 20:01:53 +0530

    Backend Practice -II

--- Database.js
+++ Database.js
@@ -1,15 +1,17 @@
 const mongoose=require('mongoose');
-require('dotenv').config();
-const Dbconnect=()=>{
+require("dotenv").config();
+const dbConnect=()=>{
     mongoose.connect(process.env.DATABASE_URL,{
         useNewUrlParser:true,
         useUnifiedTopology:true,
     })
     .then(()=>{
-        console.log("Database Connectivity is Successful");
+        console.log("DB Connection succesfully")
     })
     .catch((error)=>{
-        console.log("Issue on Connectivity");
+    console.log("Issue on connection");
+    console.error(error.message);
+    process.exit();
     });
 }
-module.exports=Dbconnect;
+module.exports = dbConnect;
